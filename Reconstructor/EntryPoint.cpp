2793c27ac33b26b11616e3f6926cbff2a8172e93
Isaac James <izach.james@gmail.com>
2014-02-15 08:20:32 -0800

    Setup opengl

--- Reconstructor/EntryPoint.cpp
+++ Reconstructor/EntryPoint.cpp
@@ -1,7 +1,9 @@
 #include "VideoToRgb.h"
+#include "GlSetup.h"
 
 int main(int argc, char **argv){
-	decode_mpeg("testVid.mp4");
+	decode_mpeg("C:\\Users\\Isaac\\Code\\Reconstructor\\Debug\\testVid.mp4");
+	testGl();
 
 	return 0;
 }
---
+++ Reconstructor/GlSetup.cpp
@@ -0,0 +1,87 @@
+#include "GlSetup.h"
+
+const GLdouble pi = 3.1415926535897932384626433832795;
+SDL_Window* displayWindow;
+SDL_Renderer* displayRenderer;
+
+void Display_InitGL()
+{
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+}
+
+int Display_SetViewport(int width, int height)
+{
+	GLfloat ratio;
+	if (height == 0) {
+		height = 1;
+	}
+
+	ratio = (GLfloat)width / (GLfloat)height;
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	GLdouble ZNear = 0.1;
+	GLdouble ZFar = 100;
+	//45 degrees = pi / 4.  Since we need left and right from 0, half that
+	GLdouble halfFOV = pi / 8;
+	GLdouble rightClip = tan(halfFOV) * ZNear;
+	GLdouble topClip = rightClip * ratio;
+	glFrustum(-rightClip, rightClip, -topClip, topClip, ZNear, ZFar);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	return 1;
+}
+
+void Display_Render()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+	glTranslatef(-1.5f, 0.0f, -6.0f);
+
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-1.0f, -1.0f, 0.0f);
+	glVertex3f(1.0f, -1.0f, 0.0f);
+	glEnd();
+
+	SDL_RenderPresent(displayRenderer);
+
+	SDL_GL_SwapWindow(displayWindow);
+}
+
+
+int testGl()
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_RendererInfo displayRendererInfo;
+	SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_OPENGL, &displayWindow, &displayRenderer);
+	SDL_GL_CreateContext(displayWindow);
+	SDL_GetRendererInfo(displayRenderer, &displayRendererInfo);
+	if ((displayRendererInfo.flags & SDL_RENDERER_ACCELERATED) == 0 ||
+		(displayRendererInfo.flags & SDL_RENDERER_TARGETTEXTURE) == 0) {
+		return -1;
+	}
+
+
+	Display_InitGL();
+
+	Display_SetViewport(800, 600);
+
+	Display_Render();
+
+
+	SDL_Delay(5000);
+	SDL_Quit();
+
+	return 0;
+}
